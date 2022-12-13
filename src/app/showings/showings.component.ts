214784d6c1345201207aaf0e5c0d2b644ab57eb3
Dominika Papierska <dominika.papierska@ppuc.pl>
2022-12-13 16:51:21 +0100

    took info about this-week into separate component

--- src/app/showings/showings.component.ts
+++ src/app/showings/showings.component.ts
@@ -19,56 +19,77 @@ export interface MovieRepertoire {
 export class ShowingsComponent implements OnInit {
   showings: MovieShowing[] = [];
   filteredShowings: MovieShowing[] = [];
-  week: string[] = [];
-  currDay: string = '';
+
   movieDetails: MovieDetails[] = [];
   description: boolean = false;
-  currentShowingInfo: unknown;
+  currDay: string = '';
+  filteredShowingsId: MovieShowing[] = [];
+  // currentShowingInfo: unknown;
 
   constructor(
     private movieApiService: MovieApiService,
-    private datesService: DatesService,
-    private currentShowing: CurrentShowingService
+    private datesService: DatesService // private currentShowing: CurrentShowingService
   ) {}
 
   ngOnInit(): void {
+    this.currDay = this.datesService.getCurrentDay();
     this.movieApiService.getMovieApiDataMovieDetails().subscribe({
       next: (response) => {
         this.movieDetails = response;
       },
     });
-    this.week = this.datesService.getCurrentWeek();
-    this.currDay = this.datesService.getCurrentDay();
+
     this.movieApiService.getMovieApiDataShowings().subscribe({
       next: (response) => {
         this.showings = response;
-        this.filteredShowings = this.showings.filter(
-          (element) => element.date == this.currDay
-        );
+        // this.filteredShowings = this.showings.filter(
+        //   (element) => element.date == this.currDay
+        // );
+        this.filterShowings(this.currDay);
       },
     });
-    this.currentShowing.currentShowingInfo$.subscribe(
-      (value) => (this.currentShowingInfo = value)
-    );
+    // this.currentShowing.currentShowingInfo$.subscribe(
+    //   (value) => (this.currentShowingInfo = value)
+    // );
+    this.movieApiService
+      .getMovieApiDataShowingForWeek('2022-12-09')
+      .subscribe({ next: (response) => console.log(response) });
   }
 
   filterShowings(day: string) {
     this.filteredShowings = this.showings.filter(
       (element) => element.date == day
     );
-  }
-
-  updateCurrentShowing(id: number) {
-    let current = this.filteredShowings.filter((element) => (element.id = id));
-    this.currentShowing.setCurrentShowing(current[0]);
-  }
-
-  compareHours(hour: string): boolean {
-    let now = new Date();
-    if (hour > `${now.getHours()}:${now.getMinutes()}`) {
-      return true;
-    } else {
-      return false;
+    if (day == this.currDay) {
+      let now = new Date();
+      this.filteredShowings = this.filteredShowings.filter(
+        (element) => element.timeFrom > `${now.getHours()}:${now.getMinutes()}`
+      );
     }
   }
+  checkIfMovieHasShowings(id: number) {
+    this.filteredShowingsId = this.filteredShowings.filter(
+      (el) => el.movieId == id
+    );
+    return this.filteredShowingsId.length > 0;
+  }
+
+  // updateCurrentShowing(id: number) {
+  //   let current = this.filteredShowings.filter((element) => (element.id = id));
+  //   this.currentShowing.setCurrentShowing(current[0]);
+  // }
+
+  // compareHours(hour: string, day: string): boolean {
+  //   let now = new Date();
+  //   const today = String(now.getDate()).padStart(2, '0');
+  //   if (
+  //     (day == `${now.getFullYear()}-${now.getMonth() + 1}-${today}` &&
+  //       hour > `${now.getHours()}:${now.getMinutes()}`) ||
+  //     day > `${now.getFullYear()}-${now.getMonth() + 1}-${today}`
+  //   ) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
 }
