fecf54399db64c323b759e790631c3b09fc770f9
Dominika Papierska <dominika.papierska@ppuc.pl>
2022-12-13 16:50:19 +0100

    added tickets component for showing seats and choosing tickets

--- src/app/tickets/tickets.component.ts
+++ src/app/tickets/tickets.component.ts
@@ -1,7 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { MovieDetails } from '../interfaces/MovieDetails';
+import { PaidSeat } from '../interfaces/PaidSeats';
+import { TicketType } from '../interfaces/TicketType';
 import { MovieApiService } from '../services/movieapi.service';
+
+interface TicketForm {
+  ticketType: FormControl<string>;
+  rowSeat: FormControl<string>;
+  columnSeat: FormControl<number>;
+}
+
+type Form2 = FormGroup<{
+  tickets: FormArray<FormGroup<TicketForm>>;
+}>;
 
 @Component({
   selector: 'app-tickets',
@@ -12,9 +30,16 @@ export class TicketsComponent implements OnInit {
   showing: any;
   movie: any;
   rows: number[] = [];
+  columns: number[] = [];
+  rowsA: string[] = [];
+  paidSeats: PaidSeat[] = [];
+  ticketsForm = this.createForm();
+  ticketTypes: TicketType[] = [];
+
   constructor(
     private route: ActivatedRoute,
-    private movieApiService: MovieApiService
+    private movieApiService: MovieApiService,
+    private builder: NonNullableFormBuilder
   ) {}
 
   ngOnInit(): void {
@@ -24,14 +49,20 @@ export class TicketsComponent implements OnInit {
     //   (element) => (this.showing = element)
     // );
     // console.log(this.showing);
+
+    this.movieApiService.getMovieApiDataTicketTypes().subscribe({
+      next: (response) => (this.ticketTypes = response),
+    });
     this.movieApiService.getMovieApiDataShowing(showingIdFromRoute).subscribe({
       next: (response) => {
         this.showing = response;
-        for (let i = 0; i < this.showing[0].rows; i++) {
-          this.rows.push(i);
-        }
-        console.log(this.rows);
-
+        this.columns = [...Array(parseInt(this.showing[0].columns)).keys()];
+        this.rows = Array.from(Array(this.showing[0].rows)).map(
+          (e, i) => i + 65
+        );
+        this.rowsA = this.rows.map((x) => String.fromCharCode(x));
+        this.paidSeats = this.showing[0].paidSeats;
+        console.log(this.paidSeats);
         this.movieApiService
           .getMovieApiDataMovie(this.showing[0].movieId)
           .subscribe({
@@ -40,6 +71,32 @@ export class TicketsComponent implements OnInit {
             },
           });
       },
+    });
+  }
+
+  checkPaidSeat(row: string, column: number) {
+    let filteredSeats = this.paidSeats.filter(
+      (el) => el.row == row && el.num == column
+    );
+    return filteredSeats.length;
+  }
+
+  addTicket(row: string, column: number) {
+    this.ticketsForm.controls.tickets.push(this.createTicketsForm(row, column));
+  }
+
+  private createForm(): Form2 {
+    const form = this.builder.group({
+      tickets: this.builder.array<FormGroup<TicketForm>>([]),
+    });
+    return form;
+  }
+
+  private createTicketsForm(row: string, column: number) {
+    return this.builder.group<TicketForm>({
+      ticketType: this.builder.control('Bilet normalny'),
+      rowSeat: this.builder.control(row),
+      columnSeat: this.builder.control(column),
     });
   }
 }
